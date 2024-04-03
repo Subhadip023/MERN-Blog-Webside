5e2d8067b8b9812b0a0749f23c37fcd9be86cc2d
Subhadip023 <subhadip240420@gmail.com>
2024-04-03 20:20:34 +0530

    fixed some bug in craete post route

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -87,7 +87,10 @@ function CreatePost() {
         setIsloading(true)
         const response = await axios.post(
           `${process.env.REACT_APP_BASE_URL}/posts`,
-          postData,
+          postData,{
+            withCredentials: true,
+            headers: { Authorization: `Bearer ${token}` },
+          }
          
         );
  
